8626ec68c50491f08f336528e7a5edf054a89804
unknown <2690926365@qq.com>
2019-11-18 19:01:28 +0800

    完成首页

--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
 // 入口文件
 import Vue from '../node_modules/vue/dist/vue.js'
 
+// 导入路由的包
+import VueRouter from "vue-router"
+
+Vue.use(VueRouter)
+
+// 导入vue-resource
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+
+
+// 导入路由模块
+import router from "./router.js"
+
 // 导入mui 的样式
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 // 导入mint-ui的组件
-import { Header } from  "mint-ui"
+import { Header,Swipe, SwipeItem } from  "mint-ui"
 
 Vue.component(Header.name , Header)
+Vue.component(Swipe.name , Swipe)
+Vue.component(SwipeItem.name , SwipeItem)
 
 import app from './App.vue'
 
 var vm = new Vue ({
     el:'#app',
-    render :c=> c(app)
+    render :c=> c(app),
+    router  //挂在路由
 })
--- src/router.js
+++ src/router.js
@@ -1,15 +1,25 @@
 import VueRouter from 'vue-router'
 
 
-// 导入两个子组件
-
+// 导入子组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes:[
+    {path:'/',redirect :'/home'},
+    {path: '/home' ,component: HomeContainer },
+    {path: '/member' ,component: MemberContainer },
+    {path: '/shopcar' ,component: ShopcarContainer },
+    {path: '/search' ,component: SearchContainer },
+
    
-  ]
+  ],
+  linkActiveClass:'mui-active'  //覆盖高亮的类
 })
 
 // 把路由对象暴露出去
